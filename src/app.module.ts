0a4fec28c194ddd52ff2cad9d10be1424385b71b
Antonette Caldwell <pullmana8@gmail.com>
2022-03-03 18:43:47 -0600

    Added the appropriate user models and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeedController } from './feed/feed.controller';
+import { FeedItem } from './models/feed-item.model';
+import { User } from './models/user.model';
 import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'testdb',
+      models: [User, FeedItem],
+    }),
+    UsersModule
+  ],
   controllers: [AppController, FeedController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
